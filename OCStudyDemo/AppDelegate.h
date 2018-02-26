bd67a7063168543dbdd866c2ca0e94a53759db1d
Albin <AlbinEmail@163.com>
2018-02-26 09:45:50 +0800

    Initial Commit

---
+++ OCStudyDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCStudyDemo
+//
+//  Created by Albin on 2018/2/26.
+//  Copyright © 2018年 Albin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCStudyDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCStudyDemo
+//
+//  Created by Albin on 2018/2/26.
+//  Copyright © 2018年 Albin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
